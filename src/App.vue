ad49491f37bf337eb93c01319b1f15fc46424fca
JustKooba <jakubsakw2009@gmail.com>
2023-03-21 19:47:45 +0100

    Set up header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,34 @@
-<template></template>
+<template>
+  <div>
+    <header-main />
+  </div>
+</template>
 
 <script>
 import axios from "axios";
-export default {};
+import HeaderMain from "./components/Header.vue";
+export default {
+  props: {
+    dictionary_api: {
+      type: String,
+      default: "https://api.dictionaryapi.dev/api/v2/entries/en/",
+    },
+  },
+  components: {
+    HeaderMain,
+  },
+  methods: {
+    async getWord() {
+      const response = await axios.get(this.dictionary_api + this.word);
+      console.log(response.data);
+    },
+  },
+  data() {
+    return {
+      word: "",
+    };
+  },
+};
 </script>
 
 <style></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <img src="../assets/logo.svg" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderMain",
+};
+</script>
+
+<style></style>
